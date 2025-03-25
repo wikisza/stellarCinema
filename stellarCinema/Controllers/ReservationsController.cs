508a7d2b5b900488f857eca3533adbecf5fe4711
WikiS <61358557+kiwimamba@users.noreply.github.com>
2025-03-25 13:29:10 +0100

    template fixed

---
+++ stellarCinema/Controllers/ReservationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace stellarCinema.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
