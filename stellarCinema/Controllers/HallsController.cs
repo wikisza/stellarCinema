2eec14fb5c2be74d9a48e25d3988f07074235c10
WikiS <61358557+kiwimamba@users.noreply.github.com>
2025-03-26 11:37:21 +0100

    seats in Book view

--- stellarCinema/Controllers/HallsController.cs
+++ stellarCinema/Controllers/HallsController.cs
@@ -30,9 +30,37 @@ namespace stellarCinema.Controllers
             {
                 _context.Add(hall);
                 await _context.SaveChangesAsync();
+                AddSeatsForHall(hall);
+                
                 return RedirectToAction(nameof(Index));
+
             }
             return View(hall);
         }
+
+        private void AddSeatsForHall(Hall hall)
+        {
+            int totalSeats = hall.TotalSeats;
+
+            int rowCount = (int)Math.Ceiling(totalSeats / 10.0);
+            List<Seat> seats = new List<Seat>();
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                char rowLetter = (char)('A' + row); 
+                for (int seatNumber = 1; seatNumber <= 10 && (row * 10 + seatNumber) <= totalSeats; seatNumber++)
+                {
+                    seats.Add(new Seat
+                    {
+                        SeatNumber = $"{rowLetter}{seatNumber}", 
+                        IdHall = hall.IdHall
+                    });
+                }
+            }
+
+            _context.Seats.AddRange(seats);
+            _context.SaveChanges();
+        }
+
     }
 }
