75af759fadacd16733c46d6b8b045efa76dffe34
WikiS <61358557+kiwimamba@users.noreply.github.com>
2025-04-05 14:50:40 +0200

    added reservation service, interface, customer panel

--- stellarCinema/Controllers/HomeController.cs
+++ stellarCinema/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using stellarCinema.Entities;
+using stellarCinema.Interfaces;
 using stellarCinema.Models;
+using static stellarCinema.Models.UserReservationViewModel;
 
 namespace stellarCinema.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationContext _context;
-
+        private readonly IReservationService _reservationService;  
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context, IReservationService reservationService)
         {
             _logger = logger;
             _context = context;
+            _reservationService = reservationService;  
         }
 
         public async Task<IActionResult> Index()
@@ -32,6 +37,19 @@ namespace stellarCinema.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult CustomerView()
+        {
+            var email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+            var model = new UserReservationsViewModel
+            {
+                CurrentReservations = _reservationService.GetCurrentReservationsForUser(email),
+                HistoryReservations = _reservationService.GetHistoryReservationsForUser(email)
+            };
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
